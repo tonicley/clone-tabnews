2b366d8ea15f924e49144963f3b51e61729df344
tonicley <tonicley@gmail.com>
2025-02-18 15:08:13 +0000

    add  endpoint

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,8 @@
+function status(request, response) {
+  //response.status(200).send("Alunos do curso.dev são pessoas acima da média.");
+  response
+    .status(200)
+    .json({ chave: "Alunos do curso.dev são pessoas acima da média." });
+}
+
+export default status;
